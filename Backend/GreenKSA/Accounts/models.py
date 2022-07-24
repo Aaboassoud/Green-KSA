7e16d2940017ba583657972c9b512d1f4834afd1
dev <abdullah.s.ageel@gmail.com>
2022-07-24 14:34:48 +0300

    test

--- Backend/GreenKSA/Accounts/models.py
+++ Backend/GreenKSA/Accounts/models.py
@@ -13,7 +13,7 @@ class Profile(models.Model):
 
 
 @receiver(post_save, sender=User)
-def create_user_profile(sender, instance, created, **kwargs):
+def create_user_profiles(sender, instance, created, **kwargs):
     if created:
         Profile.objects.create(user=instance)
 
