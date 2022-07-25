175a0d8997f3ee1988f00ec48624e14f719871ff
ahmed <a-5amd@hotmail.com>
2022-07-25 13:02:29 +0300

    update in about component

--- FrontEnd/green-k.s.a/src/Components/Poster.js
+++ FrontEnd/green-k.s.a/src/Components/Poster.js
@@ -4,7 +4,7 @@ export default function Poster() {
   return (
     <div>
       <body>
-    <div class="container">
+    <div class="container1">
         <div class="card">
             <div class="face face1">
                 <div class="content">
@@ -16,36 +16,31 @@ export default function Poster() {
                 <div class="content">
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cum cumque minus iste veritatis provident at.</p>
                         <a href="#">Read More</a>
-                </div>
+                        
+              
             </div>
         </div>
+       
+        </div>
+    </div>
+    
+    <div class="container1">
         <div class="card">
             <div class="face face1">
                 <div class="content">
-                    <img src="https://github.com/Jhonierpc/WebDevelopment/blob/master/CSS%20Card%20Hover%20Effects/img/code_128.png?raw=true"/>
-                    <h3>Code</h3>
+                    <img src="https://github.com/Jhonierpc/WebDevelopment/blob/master/CSS%20Card%20Hover%20Effects/img/design_128.png?raw=true"/>
+                    <h3>Design</h3>
                 </div>
             </div>
             <div class="face face2">
                 <div class="content">
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cum cumque minus iste veritatis provident at.</p>
                         <a href="#">Read More</a>
-                </div>
+                        <button>delete</button>
+              
             </div>
         </div>
-        <div class="card">
-            <div class="face face1">
-                <div class="content">
-                    <img src="https://github.com/Jhonierpc/WebDevelopment/blob/master/CSS%20Card%20Hover%20Effects/img/launch_128.png?raw=true"/>
-                    <h3>Launch</h3>
-                </div>
-            </div>
-            <div class="face face2">
-                <div class="content">
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cum cumque minus iste veritatis provident at.</p>
-                        <a href="#">Read More</a>
-                </div>
-            </div>
+       
         </div>
     </div>
 </body>
