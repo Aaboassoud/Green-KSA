9bf4100bf9a8480890c7ef65775c117ded91f5ef
ahmed <a-5amd@hotmail.com>
2022-07-25 23:19:43 +0300

    update in profilcom and update in app2.css and add slider

--- FrontEnd/green-k.s.a/src/Components/Pages/Profile.js
+++ FrontEnd/green-k.s.a/src/Components/Pages/Profile.js
@@ -1,7 +1,9 @@
 import React from 'react'
-
+import ProfileCmp from "../ProfileCmp"
 export default function Profile() {
   return (
-    <div>Profile</div>
+    <div>
+      <ProfileCmp/>
+    </div>
   )
 }
