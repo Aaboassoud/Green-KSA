a07e48fee35305437d592d6f2d30de64fd608802
mohamed sulaiman <mohamed@outlook.sa>
2022-07-26 14:10:53 +0300

    updated in ap.css and files.js and compnent

--- FrontEnd/green-k.s.a/src/Components/Achievements.js
+++ FrontEnd/green-k.s.a/src/Components/Achievements.js
@@ -1,54 +1,64 @@
 import React from "react";
+import { Col, Container, Row } from "react-bootstrap";
 
 
 export default function () {
   return (
-    <div className="achievement">
-      <div class="a-box cardsAchievement">
-        <div className="divAchievement">
-          <div class="img-container">
-            <div class="img-inner">
-              <div class="inner-skew">
-                <img src="https://images.unsplash.com/photo-1584791097929-aa825fe8e1e1?ixlib=rb-1.2.1&q=85&fm=jpg&crop=entropy&cs=srgb&ixid=eyJhcHBfaWQiOjE0NTg5fQ" />
+    <div>
+            <Container>
+      <Row>
+        <div class="a-box cardsAchievement">
+
+          <Col>
+            <div className="divAchievement">
+              <div class="img-container">
+                <div class="img-inner">
+                  <div class="inner-skew">
+                    <img src="https://images.unsplash.com/photo-1584791097929-aa825fe8e1e1?ixlib=rb-1.2.1&q=85&fm=jpg&crop=entropy&cs=srgb&ixid=eyJhcHBfaWQiOjE0NTg5fQ" />
+                  </div>
+                </div>
+              </div>
+              <div class="text-container">
+                <h2>عدد المناطق</h2>
+                <h2>13</h2>
               </div>
             </div>
-          </div>
-          <div class="text-container">
-            <h2>عدد المناطق</h2>
-            <h2>13</h2>
-          </div>
-        </div>
-
-        <div className="divAchievement">
-          <div class="img-container">
-            <div class="img-inner">
-              <div class="inner-skew">
-                <img src="https://images.unsplash.com/photo-1584791097929-aa825fe8e1e1?ixlib=rb-1.2.1&q=85&fm=jpg&crop=entropy&cs=srgb&ixid=eyJhcHBfaWQiOjE0NTg5fQ" />
+          </Col>
+          <Col>
+            <div className="divAchievement">
+              <div class="img-container">
+                <div class="img-inner">
+                  <div class="inner-skew">
+                    <img src="https://images.unsplash.com/photo-1584791097929-aa825fe8e1e1?ixlib=rb-1.2.1&q=85&fm=jpg&crop=entropy&cs=srgb&ixid=eyJhcHBfaWQiOjE0NTg5fQ" />
+                  </div>
+                </div>
+              </div>
+              <div class="text-container">
+                <h2>عدد المناطق</h2>
+                <h2>13</h2>
               </div>
             </div>
-          </div>
-          <div class="text-container">
-            <h2>عدد المناطق</h2>
-            <h2>13</h2>
-          </div>
-        </div>
-
-        <div className="divAchievement">
-          <div class="img-container">
-            <div class="img-inner">
-              <div class="inner-skew">
-                <img src="https://images.unsplash.com/photo-1584791097929-aa825fe8e1e1?ixlib=rb-1.2.1&q=85&fm=jpg&crop=entropy&cs=srgb&ixid=eyJhcHBfaWQiOjE0NTg5fQ" />
+          </Col>
+          <Col>
+            <div className="divAchievement">
+              <div class="img-container">
+                <div class="img-inner">
+                  <div class="inner-skew">
+                    <img src="https://images.unsplash.com/photo-1584791097929-aa825fe8e1e1?ixlib=rb-1.2.1&q=85&fm=jpg&crop=entropy&cs=srgb&ixid=eyJhcHBfaWQiOjE0NTg5fQ" />
+                  </div>
+                </div>
+              </div>
+              <div class="text-container">
+                <h2>عدد المناطق</h2>
+                <h2>13</h2>
               </div>
             </div>
-          </div>
-          <div class="text-container">
-            <h2>عدد المناطق</h2>
-            <h2>13</h2>
-          </div>
+          </Col>
+          
+          
         </div>
-
-        
-      </div>
+      </Row>
+            </Container>
     </div>
   );
 }
--- FrontEnd/green-k.s.a/src/Components/Tickets.js
+++ FrontEnd/green-k.s.a/src/Components/Tickets.js
@@ -1,18 +1,36 @@
-import React from 'react'
-import "../App2.css"
+import React from "react";
+import { Row } from "react-bootstrap";
+import "../App2.css";
 
 export default function Tickets() {
   return (
     <div>
-       
-  <div class='card1'>
-    <div class='info'>
-      <h1 class='title'>Title</h1>
-      <p class='description'>Lorem ipsum .</p>
+      <Row sm={2} md={3} className="g-1">
+        <div class="card1">
+          <div class="info">
+            <h1 class="title">Title</h1>
+            <p class="description">Lorem ipsum .</p>
+          </div>
+        </div>
+        <div class="card1">
+          <div class="info">
+            <h1 class="title">Title</h1>
+            <p class="description">Lorem ipsum .</p>
+          </div>
+        </div>
+        <div class="card1">
+          <div class="info">
+            <h1 class="title">Title</h1>
+            <p class="description">Lorem ipsum .</p>
+          </div>
+        </div>
+        <div class="card1">
+          <div class="info">
+            <h1 class="title">Title</h1>
+            <p class="description">Lorem ipsum .</p>
+          </div>
+        </div>
+      </Row>
     </div>
-  </div>
-
-
-  </div>
-  )
+  );
 }
