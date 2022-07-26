403aa9ec62bc97a4ea0e884aef8420f664a798df
mohamed sulaiman <mohamed@outlook.sa>
2022-07-26 08:05:34 +0300

    editing on home.js and app1-2.css in FrontEnd folder

--- FrontEnd/green-k.s.a/src/Components/About.js
+++ FrontEnd/green-k.s.a/src/Components/About.js
@@ -1,18 +1,19 @@
-import React from 'react'
+import React from "react";
 
 export default function About() {
   return (
     <div>
-         <div className="aboutComponent">
-      <div className="aboutContent">
-        <h1>الإلهام:</h1>
-        <br/>
-        <p>
-        
-نبعت هذه الفكرة من مشروع الرياض الخضراء بحيث تساهم هذه الفكرة بتسريع المبادرة والمساعدة في تحقيق رؤية المملكة 2030 وبدلا من ان يكون هذا المشروع في الرياض فقط يكون في جميع انحاء المملكة
-        </p>
+      <div className="aboutComponent">
+        <div className="aboutContent">
+          <h1>من نحن</h1>
+          <hr/>
+          <p>
+            نبعت هذه الفكرة من مشروع الرياض الخضراء بحيث تساهم هذه الفكرة بتسريع
+            المبادرة والمساعدة في تحقيق رؤية المملكة 2030 وبدلا من ان يكون هذا
+            المشروع في الرياض فقط يكون في جميع انحاء المملكة
+          </p>
+        </div>
       </div>
     </div>
-    </div>
-  )
+  );
 }
