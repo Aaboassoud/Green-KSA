4ade189fda2e2d59547e6cf3350edb6d95cfb85c
ahmed <a-5amd@hotmail.com>
2022-07-27 19:17:35 +0300

    add poster rate and poster user

--- FrontEnd/green-k.s.a/src/Components/PosterRater.js
+++ FrontEnd/green-k.s.a/src/Components/PosterRater.js
@@ -1,7 +1,46 @@
 import React from 'react'
+import{ Card, Button, Col, Row, Form} from 'react-bootstrap';
 
 export default function PosterRater() {
   return (
-    <div>PosterRater</div>
+    <div>
+              <Row md={"2"} >
+        <Col>
+  
+    
+            <div style={{ textAlign: "center", margin: "10%" }}>
+            
+                  <div class="b-box">
+                    <div class="img2-container">
+                      <div class="img-inner">
+                        <div class="inner1-skew">
+                          <img src="https://viewgital.com/img/chat/default-user-image.jpg" />
+                        </div>
+                      </div>
+                    </div>
+                    <div class="text-container">
+                      <h2>اليوزر</h2>
+                      <h4>المدينة</h4>
+                      <h4>نخلة</h4>
+                      <p>الوصف</p>
+                      
+                      
+                      <Form.Group as={Col} controlId="formGridPassword">
+                    <Form.Control type="text" placeholder="قيم هنا" />
+                  </Form.Group>
+                      <Button id="button" variant="primary" type="submit" size="lg">
+                قيّم
+            </Button>
+                    </div>
+                  
+                  </div>
+                  </div>
+
+                  
+                 
+      </Col>
+   
+      </Row>
+    </div>
   )
 }
--- FrontEnd/green-k.s.a/src/Components/PosterUser.js
+++ FrontEnd/green-k.s.a/src/Components/PosterUser.js
@@ -1,11 +1,14 @@
 import React from 'react'
-import{ Card, Button} from 'react-bootstrap';
+import{ Card, Button, Col, Row, Form} from 'react-bootstrap';
 
 export default function PosterUser() {
   return (
     <div>
+      
+      <Row md={"2"} >
+        <Col>
   
-          <div className="Postersuser">
+    
             <div style={{ textAlign: "center", margin: "10%" }}>
             
                   <div class="b-box">
@@ -17,12 +20,30 @@ export default function PosterUser() {
                       </div>
                     </div>
                     <div class="text-container">
-                      <h3>عدد المناطق</h3>
-                      <h2>13</h2>
+                      <h2>اليوزر</h2>
+                      <h4>المدينة</h4>
+                      <h4>نخلة</h4>
+                      <p>الوصف</p>
+                      
+                      
+                
+                      <Button id="button" variant="primary" type="submit" size="lg">
+                تعديل الحساب 
+            </Button>
+            
+                      <Button id="button" variant="primary" type="submit" size="lg">
+                حذف الحساب
+            </Button>
                     </div>
+                  
                   </div>
                   </div>
-                  </div>
+
+                  
+                 
+      </Col>
+   
+      </Row>
              
 
     </div>
