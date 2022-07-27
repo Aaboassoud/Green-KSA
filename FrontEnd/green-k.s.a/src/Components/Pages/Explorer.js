a5fbb86b22ba7e45f0b8f45d0ea804ca2b2cdf42
ahmed <a-5amd@hotmail.com>
2022-07-27 23:47:07 +0300

    update some component

--- FrontEnd/green-k.s.a/src/Components/Pages/Explorer.js
+++ FrontEnd/green-k.s.a/src/Components/Pages/Explorer.js
@@ -1,13 +1,13 @@
 import React from 'react'
+import{ Card, Button, Col, Row, Form, Collapse} from 'react-bootstrap';
+import  { useState } from 'react';
 
 
 export default function Explorer() {
+    const [open, setOpen] = useState(false);
   return (
     <div>
-  
-        
-      
-        
+
     </div>
   )
 }
