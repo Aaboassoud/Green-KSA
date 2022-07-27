65e7efc40412345e0d61770e37d25c2e37ed1e04
ahmed <a-5amd@hotmail.com>
2022-07-27 15:59:17 +0300

    creat 2 modal

--- FrontEnd/green-k.s.a/src/Components/Pages/Explorer.js
+++ FrontEnd/green-k.s.a/src/Components/Pages/Explorer.js
@@ -1,11 +1,11 @@
 import React from 'react'
-import Sidebar from '../Sidebar'
-import PosterRater from '../PosterRater'
+
 
 export default function Explorer() {
   return (
     <div>
-        <PosterRater/>
+        
+      
         
     </div>
   )
